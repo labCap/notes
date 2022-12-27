dcf205724af21f202f7bb4f281f7ba9970942a84
labCap <banlazthosow@gmail.com>
2022-12-27 18:16:44 +0200

    added jsx-structure for component Note

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { MdDeleteForever } from "react-icons/md";
 
 export const Note = () => {
-  return <div className="note"></div>;
+  return (
+    <div className="note">
+      <span>hi</span>
+      <div className="note__footer">
+        <small>13/04/2022</small>
+        <MdDeleteForever className="delete-icon" size="1.3em" />
+      </div>
+    </div>
+  );
 };
