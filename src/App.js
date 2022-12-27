92a3fbe7c4a79092f445f04e995837d976320327
labCap <banlazthosow@gmail.com>
2022-12-27 19:42:49 +0200

    added component Search to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { nanoid } from "nanoid";
 import { NoteList } from "./components/NoteList";
+import { Search } from "./components/Search";
 
 function App() {
   const [notes, setNotes] = useState([
@@ -39,6 +40,7 @@ function App() {
 
   return (
     <div className="container">
+      <Search />
       <NoteList
         notes={notes}
         handleAddNote={addNote}
